7d08ecc55cd3eb313e1d0f5b90fd820471bbab52
unknown <diliprasanna96@gmail.com>
2023-02-08 13:25:06 +0530

    update

--- 02_Spring_File_Uploader_Back-End/src/main/java/lk/ijse/spring/controller/MainFormController.java
+++ 02_Spring_File_Uploader_Back-End/src/main/java/lk/ijse/spring/controller/MainFormController.java
@@ -1,17 +1,10 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Base64;
 
 @RestController
