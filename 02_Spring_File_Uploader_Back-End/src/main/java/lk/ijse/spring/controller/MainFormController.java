f17171e13d800960cd455a351ecde7f972e8ec0c
unknown <diliprasanna96@gmail.com>
2023-02-08 10:27:25 +0530

    update

--- 02_Spring_File_Uploader_Back-End/src/main/java/lk/ijse/spring/controller/MainFormController.java
+++ 02_Spring_File_Uploader_Back-End/src/main/java/lk/ijse/spring/controller/MainFormController.java
@@ -1,4 +1,26 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/upload")
+@CrossOrigin
+@ResponseBody
 public class MainFormController {
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        try {
+            // store the uploaded file
+            file.transferTo(new File("/path/to/store/file"));
+            return ResponseEntity.ok().body("File uploaded successfully");
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().body("Failed to upload file: " + e.getMessage());
+        }
+    }
 }
